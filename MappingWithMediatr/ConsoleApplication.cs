75560d4c56cf7b71860ab087713af754b63d0c64
Dimitris <d.karkanas@gmail.com>
2021-08-13 10:55:56 +0300

    Add comments

--- MappingWithMediatr/ConsoleApplication.cs
+++ MappingWithMediatr/ConsoleApplication.cs
@@ -17,12 +17,12 @@ namespace MappingWithMediatr
         public async Task Run()
         {
             var serviceA = _services(nameof(SomeServiceA));
-            var modelA = await serviceA.GetResponseModel();
-            Console.WriteLine(modelA);
+            var modelA = await serviceA.GetResponseModel();            
+            Console.WriteLine(modelA); // Id is "1" and Description is "SomeServiceA"
 
             var serviceB = _services(nameof(SomeServiceB));
-            var modelB = await serviceB.GetResponseModel();
-            Console.WriteLine(modelB);
+            var modelB = await serviceB.GetResponseModel();            
+            Console.WriteLine(modelB); // Id is "2" and Description is "SomeServiceB"
         }
     }
 }
